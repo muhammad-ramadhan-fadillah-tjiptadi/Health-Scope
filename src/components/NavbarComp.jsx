3e79a23803d89c9d426afeeecf729c7a1db4c325
Byramadhan <rmafdllhcptd@gmail.com>
2026-02-27 11:21:27 +0700

    fix: padding and margin in app.css change to 0 for navbar

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,3 +1,4 @@
+
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 import { Link } from "react-router-dom";
 
@@ -5,7 +6,7 @@ export default function NavbarComp() {
     return (
         <Navbar fluid rounded>
             <NavbarBrand as={Link} href="https://flowbite-react.com">
-                <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                <img src="/favicon.svg" className="mr-3 h-6 sm:h-15" alt="Flowbite React Logo" />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
             </NavbarBrand>
             <NavbarToggle />
