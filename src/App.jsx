3461ed0ba65b22ff6ee58a68c880a5e77f829c87
Byramadhan <rmafdllhcptd@gmail.com>
2026-02-27 10:56:41 +0700

    add: website covid-api

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "flowbite-react"
+import './App.css'
+import NavbarComp from "./components/NavbarComp"
+
+export default function App() {
+  return (
+    <>
+      <NavbarComp />
+    </>
+  )
+}
